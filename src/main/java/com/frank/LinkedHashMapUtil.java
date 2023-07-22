751b7b199eb34234216db2c5df38aa0a125ed8c4
faxx1080 <frank.migliorino@nyu.edu>
2023-07-22 11:03:02 -0400

    Add Class

---
+++ src/main/java/com/frank/LinkedHashMapUtil.java
@@ -0,0 +1,129 @@
+package com.frank;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LinkedHashMapUtil<K, V> {
+
+    public static <K, V> LinkedHashMap<K, V> of(K k1, V v1) {
+        var lhm = new LinkedHashMap<K, V>();
+        lhm.put(k1, v1);
+        return lhm;
+    }
+
+
+    public static <K, V> LinkedHashMap<K, V> of(K k1, V v1, K k2, V v2) {
+        LinkedHashMap<K, V> lhm = new LinkedHashMap<>();
+        lhm.put(k1, v1);
+        lhm.put(k2, v2);
+        return lhm;
+    }
+
+
+    public static <K, V> LinkedHashMap<K, V> of(K k1, V v1, K k2, V v2, K k3, V v3) {
+        LinkedHashMap<K, V> lhm = new LinkedHashMap<>();
+        lhm.put(k1, v1);
+        lhm.put(k2, v2);
+        lhm.put(k3, v3);
+        return lhm;
+    }
+
+    public static <K, V> LinkedHashMap<K, V> of(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4) {
+        LinkedHashMap<K, V> lhm = new LinkedHashMap<>();
+        lhm.put(k1, v1);
+        lhm.put(k2, v2);
+        lhm.put(k3, v3);
+        lhm.put(k4, v4);
+        return lhm;
+    }
+
+    public static <K, V> LinkedHashMap<K, V> of(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5) {
+        LinkedHashMap<K, V> lhm = new LinkedHashMap<>();
+        lhm.put(k1, v1);
+        lhm.put(k2, v2);
+        lhm.put(k3, v3);
+        lhm.put(k4, v4);
+        lhm.put(k5, v5);
+        return lhm;
+    }
+
+    public static <K, V> LinkedHashMap<K, V> of(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5,
+                               K k6, V v6) {
+        LinkedHashMap<K, V> lhm = new LinkedHashMap<>();
+        lhm.put(k1, v1);
+        lhm.put(k2, v2);
+        lhm.put(k3, v3);
+        lhm.put(k4, v4);
+        lhm.put(k5, v5);
+        lhm.put(k6, v6);
+        return lhm;
+    }
+
+    public static <K, V> LinkedHashMap<K, V> of(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5,
+                               K k6, V v6, K k7, V v7) {
+        LinkedHashMap<K, V> lhm = new LinkedHashMap<>();
+        lhm.put(k1, v1);
+        lhm.put(k2, v2);
+        lhm.put(k3, v3);
+        lhm.put(k4, v4);
+        lhm.put(k5, v5);
+        lhm.put(k6, v6);
+        lhm.put(k7, v7);
+        return lhm;
+    }
+
+    public static <K, V> LinkedHashMap<K, V> of(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5,
+                               K k6, V v6, K k7, V v7, K k8, V v8) {
+        LinkedHashMap<K, V> lhm = new LinkedHashMap<>();
+        lhm.put(k1, v1);
+        lhm.put(k2, v2);
+        lhm.put(k3, v3);
+        lhm.put(k4, v4);
+        lhm.put(k5, v5);
+        lhm.put(k6, v6);
+        lhm.put(k7, v7);
+        lhm.put(k8, v8);
+        return lhm;
+    }
+
+    public static <K, V> LinkedHashMap<K, V> of(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5,
+                               K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9) {
+        LinkedHashMap<K, V> lhm = new LinkedHashMap<>();
+        lhm.put(k1, v1);
+        lhm.put(k2, v2);
+        lhm.put(k3, v3);
+        lhm.put(k4, v4);
+        lhm.put(k5, v5);
+        lhm.put(k6, v6);
+        lhm.put(k7, v7);
+        lhm.put(k8, v8);
+        lhm.put(k9, v9);
+        return lhm;
+    }
+
+    public static <K, V> LinkedHashMap<K, V> of(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5,
+                               K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9, K k10, V v10) {
+        LinkedHashMap<K, V> lhm = new LinkedHashMap<>();
+        lhm.put(k1, v1);
+        lhm.put(k2, v2);
+        lhm.put(k3, v3);
+        lhm.put(k4, v4);
+        lhm.put(k5, v5);
+        lhm.put(k6, v6);
+        lhm.put(k7, v7);
+        lhm.put(k8, v8);
+        lhm.put(k9, v9);
+        lhm.put(k10, v10);
+        return lhm;
+    }
+
+    @SafeVarargs
+    @SuppressWarnings("varargs")
+    public static <K, V> LinkedHashMap<K, V> ofEntries(Map.Entry<? extends K, ? extends V>... entries) {
+        LinkedHashMap<K, V> lhm = new LinkedHashMap<>();
+        for (var entry: entries) {
+            lhm.put(entry.getKey(), entry.getValue());
+        }
+        return lhm;
+    }
+}
--- src/main/java/com/frank/Main.java
+++ src/main/java/com/frank/Main.java
@@ -1,7 +1,11 @@
 package com.frank;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.util.*;
 
@@ -31,6 +35,30 @@ public class Main {
                 entry('n',	14),
                 entry('o',	15)
         );
+        var lmp1 = LinkedHashMapUtil.of(
+                Math.exp(50), mp
+        );
+        var lmp2 = LinkedHashMapUtil.of(
+                LocalDate.of(2020,1,1), Math.PI,
+                LocalDate.of(2021,1,1), Math.E
+        );
+        var lmp3 = LinkedHashMapUtil.ofEntries(
+                entry('a',	1),
+                entry('b',	2),
+                entry('c',	3),
+                entry('d',	4),
+                entry('e',	5),
+                entry('f',	6),
+                entry('g',	7),
+                entry('h',	8),
+                entry('i',	9),
+                entry('j',	10),
+                entry('k',	11),
+                entry('l',	12),
+                entry('m',	13),
+                entry('n',	14),
+                entry('o',	15)
+        );
         var l1 = List.of("a", "B", 3, mp, false, LocalDate.of(2023, 7, 20));
         var l2 = List.of(3,4,5);
         var l3 = List.of(false);
@@ -44,6 +72,12 @@ public class Main {
         var s1 = Set.of(1,3,5);
         var s2 = Set.of(true);
         var s3 = Collections.emptySet();
+        // print internal state
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        StatusPrinter.print(lc);
+
+        log.info("{}", mp5);
+
         log.info(
                 "Outputting:\n" +
                         "{}\n" +
@@ -59,8 +93,11 @@ public class Main {
                         "{}\n" +
                         "{}\n" +
                         "{}\n" +
+                        "{}\n" +
+                        "{}\n" +
+                        "{}\n" +
                         "{}\n",
-                mp, mp2, mp3, mp4, mp5,
+                mp, mp2, mp3, mp4, mp5, lmp1, lmp2, lmp3,
                 l1, l2, l3, l4, l5, a1,
                 s1, s2, s3);
     }
