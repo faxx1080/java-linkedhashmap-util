aed1e56a1df118eaa3d26ca53097c09265246645
faxx1080 <frank.migliorino@nyu.edu>
2023-07-22 11:10:32 -0400

    Removed non test dependency on Log

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,15 +10,11 @@ repositories {
 }
 
 dependencies {
-    implementation("ch.qos.logback:logback-classic:1.4.8")
-
     compileOnly ("org.projectlombok:lombok:1.18.28")
     annotationProcessor ("org.projectlombok:lombok:1.18.28")
-
     testCompileOnly ("org.projectlombok:lombok:1.18.28")
     testAnnotationProcessor ("org.projectlombok:lombok:1.18.28")
-
-
+    testImplementation("ch.qos.logback:logback-classic:1.4.8")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
